4ae77a6e29ac4c8d1dcf9ee49f719db590b58122
Rekha Maurya <mauryarekha795@gmail.com>
2026-05-05 02:02:52 +0530

    Fix route handling for all paths in server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/tasks", require("./routes/taskRoutes"));
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
